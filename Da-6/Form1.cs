07da04db86b07179ad35de6868fbd76b29f797c2
Konstantin8574 <160573238+Konstantin8574@users.noreply.github.com>
2024-10-10 16:36:51 +0700

    Update Form1.cs

--- Da-6/Form1.cs
+++ Da-6/Form1.cs
@@ -1,45 +1,49 @@
-using System.Windows.Forms;
+using System;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace Da_6
 {
     public partial class Form1 : Form
     {
-        Thread th;
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Обработчик кнопки для открытия Form2
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Close();
-            th = new Thread(open1);
+            // Запускаем Form2 в отдельном потоке, без закрытия Form1
+            Thread th = new Thread(openForm2);
             th.SetApartmentState(ApartmentState.STA);
             th.Start();
-
         }
 
-        public void open1(object obj)
+        // Метод для открытия Form2
+        public void openForm2()
         {
             Application.Run(new Form2());
         }
-        public void open2(object obj)
+
+        // Обработчик кнопки для открытия Form3
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Запускаем Form3 в отдельном потоке, без закрытия Form1
+            Thread th = new Thread(openForm3);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+
+        // Метод для открытия Form3
+        public void openForm3()
         {
             Application.Run(new Form3());
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Close();
-            th = new Thread(open2);
-            th.SetApartmentState(ApartmentState.STA);
-            th.Start();
-        }
-
+        // Оставляем пустую обработку для label1_Click, если она не используется
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
     }
 }
